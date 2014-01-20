5b4449ad3db59e11ba6fbd7d79e4b2140b5325d2
gdimperi <giulia.dimperio@cern.ch>
2014-01-20 23:54:25 +0100

    test GT

--- crab/createAndRunMCCrab.py
+++ crab/createAndRunMCCrab.py
@@ -11,25 +11,26 @@ datasets = {
     # Gamma + jets
     "/G_Pt-30to50_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-30to50",
     "/G_Pt-50to80_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-50to80",
-    "/G_Pt-80to120_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-80to120",
-    "/G_Pt-120to170_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-120to170",
-    "/G_Pt-170to300_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-170to300",
-    "/G_Pt-300to470_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-300to470",
-    "/G_Pt-470to800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-470to800",
-    "/G_Pt-800to1400_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-800to1400",
-    "/G_Pt-1400to1800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-1400to1800",
-    "/G_Pt-1800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-1800",
+    #"/G_Pt-80to120_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-80to120",
+    #"/G_Pt-120to170_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-120to170",
+    #"/G_Pt-170to300_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-170to300",
+    #"/G_Pt-300to470_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-300to470",
+    #"/G_Pt-470to800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-470to800",
+    #"/G_Pt-800to1400_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-800to1400",
+    #"/G_Pt-1400to1800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-1400to1800",
+    #"/G_Pt-1800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "G_Pt-1800",
 
     # QCD
-    "/QCD_Pt_30_80_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-30to80",
-    "/QCD_Pt_80_170_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-80to170",
-    "/QCD_Pt_170_250_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-170to250",
-    "/QCD_Pt_250_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-250to350",
-    "/QCD_Pt_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-350",
+    #"/QCD_Pt_30_80_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-30to80",
+    #"/QCD_Pt_80_170_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-80to170",
+    #"/QCD_Pt_170_250_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-170to250",
+    #"/QCD_Pt_250_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-250to350",
+    #"/QCD_Pt_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM": "QCD_Pt-350",
     }
 
 # Get email address
-email = "%s@ipnl.in2p3.fr" % (pwd.getpwuid(os.getuid()).pw_name)
+#email = "%s@ipnl.in2p3.fr" % (pwd.getpwuid(os.getuid()).pw_name)
+email = "giulia.dimperio@cern.ch"
 
 d = datetime.datetime.now().strftime("%d%b%y")
 
@@ -45,13 +46,15 @@ for dataset_path, dataset_name in datasets.items():
   publish_name = "%s_%s_%s-v%d" % (dataset_name, dataset_globaltag, d, version)
   ui_working_dir = ("crab_MC_%s") % (dataset_name)
   output_file = "crab_MC_%s.cfg" % (dataset_name)
+  output_dir = "store/JetMET/1stLevel/MC/%s/%s" % (d, dataset_name)
 
   print("Creating config file for '%s'" % (dataset_path))
   print("\tName: %s" % dataset_name)
   print("\tPublishing name: %s" % publish_name)
   print("")
 
-  os.system("sed -e \"s#@datasetname@#%s#\" -e \"s#@uiworkingdir@#%s#g\" -e \"s#@publish_data_name@#%s#g\" -e \"s#@email@#%s#g\" crab_MC.cfg.template.ipnl > %s" % (dataset_path, ui_working_dir, publish_name, email, output_file))
+  os.system("sed -e \"s#@datasetname@#%s#\" -e \"s#@uiworkingdir@#%s#g\" -e \"s#@publish_data_name@#%s#g\" -e \"s#@email@#%s#g\" -e \"s#@remote_dir@#%s#g\" crab_MC.cfg.template.ipnl > %s" % (dataset_path, ui_working_dir, publish_name, email, output_dir, output_file))
+  #os.system("sed -e \"s#@datasetname@#%s#\" -e \"s#@uiworkingdir@#%s#g\" -e \"s#@publish_data_name@#%s#g\" -e \"s#@email@#%s#g\" crab_MC.cfg.template.ipnl > %s" % (dataset_path, ui_working_dir, publish_name, email, output_file))
 
   if options.run:
     cmd = "crab -create -cfg %s" % (output_file)
